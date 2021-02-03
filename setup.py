e548c29fd91f85045599591f5da24db0be4aaf24
Rose Dickinson <rose.dickinson@stfc.ac.uk>
2021-02-03 10:48:57 +0000

    fix: Add i to dafni

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="dafni-cli-dafn-facility",
+    name="dafni-cli-dafni-facility",
     version="0.0.1",
     author="DAFNI Facility",
     author_email="support@dafni.ac.uk",
