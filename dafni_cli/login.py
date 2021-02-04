06652b91a2c798390301a6297fbb09930e183fef
roseDickinson <47528557+roseDickinson@users.noreply.github.com>
2021-02-04 11:59:54 +0000

    feat: Add CLI to login and Upload a Model and add Workflow to auto publish (#1)

---
+++ dafni_cli/login.py
@@ -0,0 +1,14 @@
+import requests
+from dafni_cli.urls import LOGIN_API_URL
+
+
+def login(username: str, password: str) -> str:
+    login_resp = requests.post(
+        f"{LOGIN_API_URL}/login/",
+        json={"username": username, "password": password},
+        headers={
+            "Content-Type": "application/json",
+        },
+        allow_redirects=False,
+    )
+    return f"JWT {login_resp.cookies['__Secure-dafnijwt']}"
---
+++ setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="dafni-cli-dafni-facility",
+    version="0.0.1",
+    author="DAFNI Facility",
+    author_email="support@dafni.ac.uk",
+    description="The beginnings of a command line interface for DAFNI. This package is still in development and not ready for use.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/dafnifacility/command-line-interface/",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.9',
+)
